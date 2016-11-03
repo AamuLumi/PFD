bb403c11eff48cad7e36262abad7534e10d92619
aamulumi <florian@kauder.fr>
2016-11-03 22:46:05 +0100

    feat(app): Add Authorization token storage and /user/create to create a user

--- server/config/express.js
+++ server/config/express.js
@@ -39,7 +39,7 @@ module.exports = function (app, config) {
     app.use(function (req, res, next) {
         res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-        res.header('Access-Control-Allow-Headers', 'Content-Type');
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
         next();
     });
