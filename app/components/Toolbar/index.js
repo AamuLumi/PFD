6e5d654130d3ccc23227ce3c20cf2e6cb3039e8f
aamulumi <florian@kauder.fr>
2016-12-02 05:31:33 +0100

    feat(app): Add cookie session storage

--- app/components/Toolbar/index.js
+++ app/components/Toolbar/index.js
@@ -11,7 +11,7 @@ class Toolbar extends Component {
     };
 
     static propTypes = {
-        loginResult: React.PropTypes.object.isRequired,
+        loggedUser: React.PropTypes.object.isRequired,
         logout: React.PropTypes.func.isRequired
     };
 
@@ -24,9 +24,10 @@ class Toolbar extends Component {
     }
 
     componentWillReceiveProps(newProps) {
-        let {loginResult} = newProps;
+        let {loggedUser} = newProps;
 
-        if (loginResult.loaded && loginResult.data && !loginResult.error) {
+        if (loggedUser.loaded && loggedUser.data &&
+            loggedUser.data.email && !loggedUser.error) {
             this.setState({
                 logged: true
             });
@@ -72,7 +73,7 @@ class Toolbar extends Component {
                             <span>Sprints</span>
                         </Link>
                         <Link to="/project">
-                            <span>Projets</span>
+                            <span style={{paddingLeft: '8px'}}>Projets</span>
                         </Link>
                         <Link to="/project/create">
                             <span style={{paddingLeft: '8px'}}>+</span>
@@ -90,7 +91,7 @@ class Toolbar extends Component {
 
 function mapStateToProps(state) {
     return {
-        loginResult: state.loginResult
+        loggedUser: state.loggedUser
     };
 }
 
---
+++ app/tools/Cookie.js
@@ -0,0 +1,25 @@
+export function getCookie(key) {
+    let name = key + '=';
+    let ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) === 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return '';
+}
+
+export function setCookie(key, value) {
+    let expires = new Date();
+    expires.setTime(expires.getTime() + (60 * 24 * 60 * 60 * 1000));
+    document.cookie = key + '=' + value + '; expires=' + expires.toUTCString()
+        + ';path=/';
+}
+
+export function removeCookie(key) {
+    document.cookie = key + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/';
+}
